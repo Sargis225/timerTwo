e3876d725f4b91f71e0dbead598635230d03d101
Sargis <sargisske@mail.ru>
2021-01-13 17:48:55 +0400

    change timer game

---
+++ timerTwo/LavelsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LavelsTableViewCell.swift
+//  timerTwo
+//
+//  Created by Saq on 1/13/21.
+//
+
+import UIKit
+
+class LavelsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- timerTwo/ViewController.swift
+++ timerTwo/ViewController.swift
@@ -7,10 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lavelNamesArray.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let indent = "lavelsIndent"
+        let cell = tableView.dequeueReusableCell(withIdentifier: indent) as! LavelsTableViewCell
+        
+        cell.textLabel?.text = lavelNamesArray[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.font = UIFont(name: .init(), size: 40)
+        cell.textLabel?.textColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        cell.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        return cell
+    }
+    let lavelNamesArray = ["EASY","AVERAGE","ROOT","PERCENT","HARD"]
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            performSegue(withIdentifier: "easyIndent", sender: nil)
+        case 1:
+            performSegue(withIdentifier: "averangeIndent", sender: nil)
+        case 2:
+            performSegue(withIdentifier: "rootIdent", sender: nil)
+        case 3:
+            performSegue(withIdentifier: "percentIndent", sender: nil)
+        case 4:
+            performSegue(withIdentifier: "hardIndent", sender: nil)
+        default:
+            print("ok")
+        }
+    }
+    
+    
+    
+    
+    @IBOutlet var lavelTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        lavelTableView.delegate = self
+        lavelTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     override func viewDidDisappear(_ animated: Bool) {
